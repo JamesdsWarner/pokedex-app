d5d9c70646307a16258566b1e275eaa8fe6b33f3
JamesdsWarner <jamesdswarner@gmail.com>
2022-12-28 20:26:00 +0000

    improving performance

--- src/components/HeaderImage/HeaderImage.component.jsx
+++ src/components/HeaderImage/HeaderImage.component.jsx
@@ -3,7 +3,10 @@ import * as Styled from "./HeaderImage.styled";
 const HeaderImage = () => {
   return (
     <Styled.HeaderImageWrapper>
-      <Styled.HeaderImage src="https://user-images.githubusercontent.com/29473781/180619084-a56960ab-7efa-4e34-9d33-4e3e581d62ff.png" />
+      <Styled.HeaderImage
+        alt="Pokedex logo"
+        src="https://user-images.githubusercontent.com/29473781/180619084-a56960ab-7efa-4e34-9d33-4e3e581d62ff.png"
+      />
     </Styled.HeaderImageWrapper>
   );
 };
--- src/components/Sprite/Sprite.component.jsx
+++ src/components/Sprite/Sprite.component.jsx
@@ -3,7 +3,7 @@ import * as Styled from "./Sprite.styles";
 const Sprite = ({ image }) => {
   return (
     <Styled.SpriteWrapper>
-      <Styled.SpriteImage src={image} />
+      <Styled.SpriteImage alt="Pokemon image" src={image} />
     </Styled.SpriteWrapper>
   );
 };
